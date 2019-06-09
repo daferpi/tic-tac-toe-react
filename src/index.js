14a650ae2106b89823847234a1243c8a4fe67219
daferpi <daferpi@protonmail.ch>
2019-06-09 13:28:32 +0200

    add function to calculate the winner

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.isXnext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -37,8 +40,36 @@ class Board extends React.Component {
       />);
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+
+
   render() {
-    const status = 'Next player: ' + (this.state.isXnext ? 'X' : 'O');
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.isXnext ? 'X' : 'O');
+    }
 
     return (
       <div>
